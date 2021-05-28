cf8c6a325f3b66bd1a4701d5a933100d5293b356
Cao0220 <24272726@qq.com>
2021-05-28 16:46:49 +0800

    .

--- fuck.js
+++ fuck.js
@@ -4,37 +4,37 @@ e.setAttribute("src", "https://cdn.jsdelivr.net/gh/Cao0220/jerryisveryhandsome@H
 document.getElementsByTagName("body")[0].insertAdjacentElement("afterbegin",
     e)
 
-const e = document.createElement("script");
-e.setAttribute("type", "text/javascript");
-e.setAttribute("src", "https://cdn.jsdelivr.net/npm/promise-polyfill@8/dist/polyfill.min.js");
+const e1 = document.createElement("script");
+e1.setAttribute("type", "text/javascript");
+e1.setAttribute("src", "https://cdn.jsdelivr.net/npm/promise-polyfill@8/dist/polyfill.min.js");
 document.getElementsByTagName("body")[0].insertAdjacentElement("afterbegin",
-    e)
+    e1)
 
 
-const e = document.createElement("script");
-e.setAttribute("type", "text/javascript");
-e.setAttribute("src", "https://cdn.jsdelivr.net/npm/howler@2.1.3/dist/howler.min.js");
+const e2 = document.createElement("script");
+e2.setAttribute("type", "text/javascript");
+e2.setAttribute("src", "https://cdn.jsdelivr.net/npm/howler@2.1.3/dist/howler.min.js");
 document.getElementsByTagName("body")[0].insertAdjacentElement("afterbegin",
-    e)
+    e2)
 
 
-const e = document.createElement("script");
-e.setAttribute("type", "text/javascript");
-e.setAttribute("src", "https://cubism.live2d.com/sdk-web/cubismcore/live2dcubismcore.min.js");
+const e3 = document.createElement("script");
+e3.setAttribute("type", "text/javascript");
+e3.setAttribute("src", "https://cubism.live2d.com/sdk-web/cubismcore/live2dcubismcore.min.js");
 document.getElementsByTagName("body")[0].insertAdjacentElement("afterbegin",
-    e)
+    e3)
 
-const e = document.createElement("script");
-e.setAttribute("type", "text/javascript");
-e.setAttribute("src", "https://cdn.jsdelivr.net/npm/pixi.js@4.6.1/dist/pixi.min.js");
+const e4 = document.createElement("script");
+e4.setAttribute("type", "text/javascript");
+e4.setAttribute("src", "https://cdn.jsdelivr.net/npm/pixi.js@4.6.1/dist/pixi.min.js");
 document.getElementsByTagName("body")[0].insertAdjacentElement("afterbegin",
-    e)
+    e4)
 
-const e = document.createElement("script");
-e.setAttribute("type", "text/javascript");
-e.setAttribute("src", "https://cdn.jsdelivr.net/gh/Cao0220/jerryisveryhandsome@HEAD/live2dv3.js");
+const e5 = document.createElement("script");
+e5.setAttribute("type", "text/javascript");
+e5.setAttribute("src", "https://cdn.jsdelivr.net/gh/Cao0220/jerryisveryhandsome@HEAD/live2dv3.js");
 document.getElementsByTagName("body")[0].insertAdjacentElement("afterbegin",
-    e)
+    e5)
 
 window.onload = () => {
     new l2dViewer({
