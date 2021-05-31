43498fe4cc045c489930299d3248387549bc6136
Cao0220 <24272726@qq.com>
2021-05-31 12:39:06 +0800

    .

--- fuck.js
+++ fuck.js
@@ -1,9 +1,3 @@
-const e = document.createElement("script");
-e.setAttribute("type", "text/javascript");
-e.setAttribute("src", "https://cdn.jsdelivr.net/gh/Cao0220/jerryisveryhandsome@HEAD/live2dv3.js");
-document.getElementsByTagName("body")[0].insertAdjacentElement("afterbegin",
-    e)
-
 const e1 = document.createElement("script");
 e1.setAttribute("type", "text/javascript");
 e1.setAttribute("src", "https://cdn.jsdelivr.net/npm/promise-polyfill@8/dist/polyfill.min.js");
@@ -18,17 +12,18 @@ document.getElementsByTagName("body")[0].insertAdjacentElement("afterbegin",
     e2)
 
 
-const e3 = document.createElement("script");
-e3.setAttribute("type", "text/javascript");
-e3.setAttribute("src", "https://cubism.live2d.com/sdk-web/cubismcore/live2dcubismcore.min.js");
-document.getElementsByTagName("body")[0].insertAdjacentElement("afterbegin",
-    e3)
-
 const e4 = document.createElement("script");
 e4.setAttribute("type", "text/javascript");
 e4.setAttribute("src", "https://cdn.jsdelivr.net/npm/pixi.js@4.6.1/dist/pixi.min.js");
 document.getElementsByTagName("body")[0].insertAdjacentElement("afterbegin",
     e4)
+
+
+const e3 = document.createElement("script");
+e3.setAttribute("type", "text/javascript");
+e3.setAttribute("src", "https://cubism.live2d.com/sdk-web/cubismcore/live2dcubismcore.min.js");
+document.getElementsByTagName("body")[0].insertAdjacentElement("afterbegin",
+    e3)
 
 const e5 = document.createElement("script");
 e5.setAttribute("type", "text/javascript");
