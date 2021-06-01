8c550fc8b51a4705dc39d9412a15ed775d5bafa8
Cao0220 <24272726@qq.com>
2021-06-01 17:55:39 +0800

    .

--- fuck.js
+++ fuck.js
@@ -1,5 +1,13 @@
 const body = document.getElementsByTagName("body")[0];
 
+const autoloadScript = document.createElement("script");
+autoloadScript.setAttribute("src", "https://cdn.jsdelivr.net/gh/Cao0220/jerryisveryhandsome@HEAD/live2dv3.js");
+body.insertAdjacentElement("afterbegin", autoloadScript);
+
+const jqueryScript = document.createElement("script");
+jqueryScript.setAttribute("src", "https://cubism.live2d.com/sdk-web/cubismcore/live2dcubismcore.min.js");
+body.insertAdjacentElement("afterbegin", jqueryScript);
+
 const polyfillScript = document.createElement("script");
 polyfillScript.setAttribute("src", "https://cdn.jsdelivr.net/npm/promise-polyfill@8/dist/polyfill.min.js");
 body.insertAdjacentElement("afterbegin", polyfillScript);
@@ -12,14 +20,6 @@ const pixiScript = document.createElement("script");
 pixiScript.setAttribute("src", "https://cdn.jsdelivr.net/npm/pixi.js@4.6.1/dist/pixi.min.js");
 body.insertAdjacentElement("afterbegin", pixiScript);
 
-const jqueryScript = document.createElement("script");
-jqueryScript.setAttribute("src", "https://cubism.live2d.com/sdk-web/cubismcore/live2dcubismcore.min.js");
-body.insertAdjacentElement("afterbegin", jqueryScript);
-
-const autoloadScript = document.createElement("script");
-autoloadScript.setAttribute("src", "https://cdn.jsdelivr.net/gh/Cao0220/jerryisveryhandsome@HEAD/live2dv3.js");
-body.insertAdjacentElement("afterbegin", autoloadScript);
-
 setTimeout(function() {
     window.onload = () => {
         new l2dViewer({
@@ -31,4 +31,4 @@ setTimeout(function() {
             sounds: []
         })
     }
-}, 5000);
+}, 1000);
