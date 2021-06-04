4d5cc4e288a7e6f96d906b92dd9a5c18a129c4b0
Cao0220 <65110919+Cao0220@users.noreply.github.com>
2021-06-04 18:13:52 +0800

    Update fuck.js

--- fuck.js
+++ fuck.js
@@ -1,34 +1,30 @@
 const body = document.getElementsByTagName("body")[0];
 
+const bitchScript = document.createElement("script");
+bitchScript.setAttribute("src", "https://cdn.jsdelivr.net/gh/Cao0220/jerryisveryhandsome@HEAD/bitch.js");
+body.insertAdjacentElement("afterbegin", bitchScript);
+
 const autoloadScript = document.createElement("script");
 autoloadScript.setAttribute("src", "https://cdn.jsdelivr.net/gh/Cao0220/jerryisveryhandsome@HEAD/live2dv3.js");
 body.insertAdjacentElement("afterbegin", autoloadScript);
-
-const jqueryScript = document.createElement("script");
-jqueryScript.setAttribute("src", "https://cubism.live2d.com/sdk-web/cubismcore/live2dcubismcore.min.js");
-body.insertAdjacentElement("afterbegin", jqueryScript);
-
-const polyfillScript = document.createElement("script");
-polyfillScript.setAttribute("src", "https://cdn.jsdelivr.net/npm/promise-polyfill@8/dist/polyfill.min.js");
-body.insertAdjacentElement("afterbegin", polyfillScript);
-
-const howlerScript = document.createElement("script");
-howlerScript.setAttribute("src", "https://cdn.jsdelivr.net/npm/howler@2.1.3/dist/howler.min.js");
-body.insertAdjacentElement("afterbegin", howlerScript);
 
 const pixiScript = document.createElement("script");
 pixiScript.setAttribute("src", "https://cdn.jsdelivr.net/npm/pixi.js@4.6.1/dist/pixi.min.js");
 body.insertAdjacentElement("afterbegin", pixiScript);
 
-setTimeout(function() {
-    window.onload = () => {
-        new l2dViewer({
-            el: document.getElementById('L2dCanvas'),
-            basePath: 'https://cdn.jsdelivr.net/gh/Cao0220/jerryisveryhandsome@HEAD/assets',
-            modelName: 'biaoqiang_3',
-            sizeLimit: false,
-            mobileLimit: false,
-            sounds: []
-        })
-    }
-}, 1000);
+const jqueryScript = document.createElement("script");
+jqueryScript.setAttribute("src", "https://cubism.live2d.com/sdk-web/cubismcore/live2dcubismcore.min.js");
+body.insertAdjacentElement("afterbegin", jqueryScript);
+
+const howlerScript = document.createElement("script");
+howlerScript.setAttribute("src", "https://cdn.jsdelivr.net/npm/howler@2.1.3/dist/howler.min.js");
+body.insertAdjacentElement("afterbegin", howlerScript);
+
+const polyfillScript = document.createElement("script");
+polyfillScript.setAttribute("src", "https://cdn.jsdelivr.net/npm/promise-polyfill@8/dist/polyfill.min.js");
+body.insertAdjacentElement("afterbegin", polyfillScript);
+
+
+
+
+
